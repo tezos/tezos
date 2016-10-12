7f9cb3e18cf7f0859b2e7d59c1b806bd2eafa3fb
Dynamic Ledger Solutions, Inc <contact@tezos.com>
2016-10-12 15:00:19 +0200

    Test: improve report

--- test/test_context.ml
+++ test/test_context.ml
@@ -1,3 +1,12 @@
+(**************************************************************************)
+(*                                                                        *)
+(*    Copyright (c) 2014 - 2016.                                          *)
+(*    Dynamic Ledger Solutions, Inc. <contact@tezos.com>                  *)
+(*                                                                        *)
+(*    All rights reserved. No warranty, explicit or implicit, provided.   *)
+(*                                                                        *)
+(**************************************************************************)
+
 open Hash
 open Context
 
