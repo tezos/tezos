a0109ad16bee61bff4a25fa93201b1a2771c9e79
Dynamic Ledger Solutions, Inc <contact@tezos.com>
2017-01-14 13:14:07 +0100

    Shell: implement `P2p_{connection_pool,welcome}`

---
+++ src/node/net/p2p_connection_pool_types.mli
@@ -0,0 +1,265 @@
+(**************************************************************************)
+(*                                                                        *)
+(*    Copyright (c) 2014 - 2016.                                          *)
+(*    Dynamic Ledger Solutions, Inc. <contact@tezos.com>                  *)
+(*                                                                        *)
+(*    All rights reserved. No warranty, explicit or implicit, provided.   *)
+(*                                                                        *)
+(**************************************************************************)
+
+open P2p_types
+
+module Point_info : sig
+
+  type 'conn t
+  type 'conn point_info = 'conn t
+  (** Type of info associated to a point. *)
+
+  val compare : 'conn point_info -> 'conn point_info -> int
+
+  type greylisting_config = {
+    factor: float ;
+    initial_delay: int ;
+    disconnection_delay: int ;
+  }
+
+  val create :
+    ?trusted:bool ->
+    ?greylisting_config:greylisting_config ->
+    addr -> port -> 'conn point_info
+  (** [create ~trusted addr port] is a freshly minted point_info. If
+      [trusted] is true, this point is considered trusted and will
+      be treated as such. *)
+
+  val trusted : 'conn point_info -> bool
+  (** [trusted pi] is [true] iff [pi] has is trusted,
+      i.e. "whitelisted". *)
+
+  val set_trusted : 'conn point_info -> unit
+  val unset_trusted : 'conn point_info -> unit
+
+  val last_failed_connection :
+    'conn point_info -> Time.t option
+  val last_rejected_connection :
+    'conn point_info -> (Gid.t * Time.t) option
+  val last_established_connection :
+    'conn point_info -> (Gid.t * Time.t) option
+  val last_disconnection :
+    'conn point_info -> (Gid.t * Time.t) option
+
+  val last_seen :
+    'conn point_info -> (Gid.t * Time.t) option
+  (** [last_seen pi] is the most recent of:
+
+      * last established connection
+      * last rejected connection
+      * last disconnection
+  *)
+
+  val last_miss :
+    'conn point_info -> Time.t option
+
+  val greylisted :
+    ?now:Time.t -> 'conn point_info -> bool
+
+  val point : 'conn point_info -> Point.t
+
+  module State : sig
+
+    type 'conn t =
+      | Requested of { cancel: Canceler.t }
+        (** We initiated a connection. *)
+      | Accepted of { current_gid: Gid.t ;
+                      cancel: Canceler.t }
+        (** We accepted a incoming connection. *)
+      | Running of { data: 'conn ;
+                     current_gid: Gid.t }
+        (** Successfully authentificated connection, normal business. *)
+      | Disconnected
+        (** No connection established currently. *)
+    type 'conn state = 'conn t
+
+    val pp : Format.formatter -> 'conn t -> unit
+
+    val get : 'conn point_info -> 'conn state
+
+    val is_disconnected : 'conn point_info -> bool
+
+    val set_requested :
+      ?timestamp:Time.t ->
+      'conn point_info -> Canceler.t -> unit
+
+    val set_accepted :
+      ?timestamp:Time.t ->
+      'conn point_info -> Gid.t -> Canceler.t -> unit
+
+    val set_running :
+      ?timestamp:Time.t -> 'conn point_info -> Gid.t -> 'conn -> unit
+
+    val set_disconnected :
+      ?timestamp:Time.t -> ?requested:bool -> 'conn point_info -> unit
+
+  end
+
+  module Event : sig
+
+    type kind =
+      | Outgoing_request
+        (** We initiated a connection. *)
+      | Accepting_request of Gid.t
+        (** We accepted a connection after authentifying the remote peer. *)
+      | Rejecting_request of Gid.t
+        (** We rejected a connection after authentifying the remote peer. *)
+      | Request_rejected of Gid.t option
+        (** The remote peer rejected our connection. *)
+      | Connection_established of Gid.t
+        (** We succesfully established a authentified connection. *)
+      | Disconnection of Gid.t
+        (** We decided to close the connection. *)
+      | External_disconnection of Gid.t
+        (** The connection was closed for external reason. *)
+
+    type t = {
+      kind : kind ;
+      timestamp : Time.t ;
+    }
+
+  end
+
+  val fold_events :
+    'conn point_info -> init:'a -> f:('a -> Event.t -> 'a) -> 'a
+
+  val log_incoming_rejection :
+    ?timestamp:Time.t -> 'conn point_info -> Gid.t -> unit
+
+end
+
+
+(** Gid info: current and historical information about a gid *)
+
+module Gid_info : sig
+
+  type ('conn, 'meta) t
+  type ('conn, 'meta) gid_info = ('conn, 'meta) t
+
+  val compare : ('conn, 'meta) t -> ('conn, 'meta) t -> int
+
+  val create :
+    ?trusted:bool ->
+    metadata:'meta ->
+    Gid.t -> ('conn, 'meta) gid_info
+  (** [create ~trusted ~meta gid] is a freshly minted gid info for
+      [gid]. *)
+
+  val gid : ('conn, 'meta) gid_info -> Gid.t
+
+  val metadata : ('conn, 'meta) gid_info -> 'meta
+  val set_metadata : ('conn, 'meta) gid_info -> 'meta -> unit
+
+  val trusted : ('conn, 'meta) gid_info -> bool
+  val set_trusted : ('conn, 'meta) gid_info -> unit
+  val unset_trusted : ('conn, 'meta) gid_info -> unit
+
+  val last_failed_connection :
+    ('conn, 'meta) gid_info -> (Id_point.t * Time.t) option
+  val last_rejected_connection :
+    ('conn, 'meta) gid_info -> (Id_point.t * Time.t) option
+  val last_established_connection :
+    ('conn, 'meta) gid_info -> (Id_point.t * Time.t) option
+  val last_disconnection :
+    ('conn, 'meta) gid_info -> (Id_point.t * Time.t) option
+
+  val last_seen :
+    ('conn, 'meta) gid_info -> (Id_point.t * Time.t) option
+  (** [last_seen gi] is the most recent of:
+
+      * last established connection
+      * last rejected connection
+      * last disconnection
+  *)
+
+  val last_miss :
+    ('conn, 'meta) gid_info -> (Id_point.t * Time.t) option
+  (** [last_miss gi] is the most recent of:
+
+      * last failed connection
+      * last rejected connection
+      * last disconnection
+  *)
+
+  module State : sig
+
+    type 'conn t =
+      | Accepted of { current_point: Id_point.t ;
+                      cancel: Canceler.t }
+        (** We accepted a incoming connection, we greeted back and
+            we are waiting for an acknowledgement. *)
+      | Running of { data: 'conn ;
+                     current_point: Id_point.t }
+        (** Successfully authentificated connection, normal business. *)
+      | Disconnected
+        (** No connection established currently. *)
+    type 'conn state = 'conn t
+
+    val pp : Format.formatter -> 'conn t -> unit
+
+    val get : ('conn, 'meta) gid_info -> 'conn state
+
+    val is_disconnected : ('conn, 'meta) gid_info -> bool
+
+    val set_accepted :
+      ?timestamp:Time.t ->
+      ('conn, 'meta) gid_info -> Id_point.t -> Canceler.t -> unit
+
+    val set_running :
+      ?timestamp:Time.t ->
+      ('conn, 'meta) gid_info -> Id_point.t -> 'conn -> unit
+
+    val set_disconnected :
+      ?timestamp:Time.t ->
+      ?requested:bool ->
+      ('conn, 'meta) gid_info -> unit
+
+  end
+
+  module Event : sig
+
+    type kind =
+      | Accepting_request
+        (** We accepted a connection after authentifying the remote peer. *)
+      | Rejecting_request
+        (** We rejected a connection after authentifying the remote peer. *)
+      | Request_rejected
+        (** The remote peer rejected our connection. *)
+      | Connection_established
+        (** We succesfully established a authentified connection. *)
+      | Disconnection
+        (** We decided to close the connection. *)
+      | External_disconnection
+        (** The connection was closed for external reason. *)
+
+    type t = {
+      kind : kind ;
+      timestamp : Time.t ;
+      point : Id_point.t ;
+    }
+
+  end
+
+  val fold_events :
+    ('conn, 'meta) gid_info -> init:'a -> f:('a -> Event.t -> 'a) -> 'a
+
+  val log_incoming_rejection :
+    ?timestamp:Time.t ->
+    ('conn, 'meta) gid_info -> Id_point.t -> unit
+
+  module File : sig
+    val load :
+      string -> 'meta Data_encoding.t ->
+      ('conn, 'meta) gid_info list Lwt.t
+    val save :
+      string -> 'meta Data_encoding.t ->
+      ('conn, 'meta) gid_info list -> bool Lwt.t
+  end
+
+end
---
+++ test/test_p2p_connection_pool.ml
@@ -0,0 +1,196 @@
+(**************************************************************************)
+(*                                                                        *)
+(*    Copyright (c) 2014 - 2016.                                          *)
+(*    Dynamic Ledger Solutions, Inc. <contact@tezos.com>                  *)
+(*                                                                        *)
+(*    All rights reserved. No warranty, explicit or implicit, provided.   *)
+(*                                                                        *)
+(**************************************************************************)
+
+open Error_monad
+open P2p_types
+include Logging.Make (struct let name = "test-p2p-connection-pool" end)
+
+type message =
+  | Ping
+
+
+let msg_config : message P2p_connection_pool.message_config = {
+  encoding = [
+    P2p_connection_pool.Encoding {
+      tag = 0x10 ;
+      encoding = Data_encoding.empty ;
+      wrap = (function () -> Ping) ;
+      unwrap = (function Ping -> Some ()) ;
+      max_length = None ;
+    } ;
+  ] ;
+  versions =  Version.[ { name = "TEST" ; major = 0 ; minor = 0 } ] ;
+}
+
+type metadata = unit
+
+let meta_config : metadata P2p_connection_pool.meta_config = {
+  encoding = Data_encoding.empty ;
+  initial = () ;
+}
+
+let rec connect ~timeout pool point =
+  lwt_log_info "Connect to %a" Point.pp point >>= fun () ->
+  P2p_connection_pool.connect pool point ~timeout >>= function
+  | Error [P2p_connection_pool.Connected] -> begin
+      match P2p_connection_pool.Points.find_connection pool point with
+      | Some conn -> return conn
+      | None -> failwith "Woops..."
+    end
+  | Error ([ P2p_connection_pool.Connection_refused
+           | P2p_connection_pool.Pending_connection
+           | P2p_connection.Rejected
+           | Lwt_utils.Canceled
+           | Lwt_utils.Timeout
+           | P2p_connection_pool.Rejected _
+           ] as err) ->
+      lwt_log_info "@[Connection to %a failed:@ %a@]"
+        Point.pp point pp_print_error err >>= fun () ->
+      Lwt_unix.sleep (0.5 +. Random.float 2.) >>= fun () ->
+      connect ~timeout pool point
+  | Ok _ | Error _ as res -> Lwt.return res
+
+let connect_all ~timeout pool points =
+  map_p (connect ~timeout pool) points
+
+type error += Connect | Write | Read
+
+let write_all conns msg  =
+  iter_p
+    (fun conn ->
+       trace Write @@ P2p_connection_pool.write_sync conn msg)
+    conns
+
+let read_all conns =
+  iter_p
+    (fun conn ->
+       trace Read @@ P2p_connection_pool.read conn >>=? fun Ping ->
+       return ())
+    conns
+
+let rec connect_random pool total rem point n =
+  Lwt_unix.sleep (0.2 +. Random.float 1.0) >>= fun () ->
+  (trace Connect @@ connect ~timeout:2. pool point) >>=? fun conn ->
+  (trace Write @@ P2p_connection_pool.write conn Ping) >>= fun _ ->
+  (trace Read @@ P2p_connection_pool.read conn) >>=? fun Ping ->
+  Lwt_unix.sleep (0.2 +. Random.float 1.0) >>= fun () ->
+  P2p_connection_pool.disconnect conn >>= fun () ->
+  begin
+    decr rem ;
+    if !rem mod total = 0 then
+      lwt_log_notice "Remaining: %d." (!rem / total)
+    else
+      Lwt.return ()
+  end >>= fun () ->
+  if n > 1 then
+    connect_random pool total rem point (pred n)
+  else
+    return ()
+
+let connect_random_all pool points n =
+  let total = List.length points in
+  let rem = ref (n * total) in
+  iter_p (fun point -> connect_random pool total rem point n) points
+
+let close_all conns =
+  Lwt_list.iter_p P2p_connection_pool.disconnect conns
+
+
+let run_net config repeat points addr port =
+  Lwt_unix.sleep (Random.float 2.0) >>= fun () ->
+  let sched = P2p_io_scheduler.create ~read_buffer_size:(1 lsl 12) () in
+  P2p_connection_pool.create
+    config meta_config msg_config sched >>= fun pool ->
+  P2p_welcome.run ~backlog:10 pool ~addr port >>= fun welcome ->
+  connect_all ~timeout:2. pool points >>=? fun conns ->
+  lwt_log_notice "Bootstrap OK" >>= fun () ->
+  write_all conns Ping >>=? fun () ->
+  lwt_log_notice "Sent all messages." >>= fun () ->
+  read_all conns >>=? fun () ->
+  lwt_log_notice "Read all messages." >>= fun () ->
+  close_all conns >>= fun () ->
+  lwt_log_notice "Begin random connections." >>= fun () ->
+  connect_random_all pool points repeat >>=? fun () ->
+  lwt_log_notice "Shutting down" >>= fun () ->
+  P2p_welcome.shutdown welcome >>= fun () ->
+  P2p_connection_pool.destroy pool >>= fun () ->
+  P2p_io_scheduler.shutdown sched >>= fun () ->
+  lwt_log_notice "Shutdown Ok" >>= fun () ->
+  return ()
+
+let make_net points repeat n =
+  let point, points = Utils.select n points in
+  let proof_of_work_target = Crypto_box.make_target [] in
+  let identity = Identity.generate proof_of_work_target in
+  let config = P2p_connection_pool.{
+    identity ;
+    proof_of_work_target ;
+    trusted_points = points ;
+    peers_file = "/dev/null" ;
+    closed_network = true ;
+    listening_port = Some (snd point) ;
+    min_connections = List.length points ;
+    max_connections = List.length points ;
+    max_incoming_connections = List.length points ;
+    authentification_timeout = 2. ;
+    incoming_app_message_queue_size = None ;
+    incoming_message_queue_size = None ;
+    outgoing_message_queue_size = None ;
+    } in
+  Process.detach
+    ~prefix:(Format.asprintf "%a " Gid.pp identity.gid)
+    begin fun () ->
+      run_net config repeat points (fst point) (snd point) >>= function
+      | Ok () -> Lwt.return_unit
+      | Error err ->
+          lwt_log_error "@[<v 2>Unexpected error: %d@ %a@]"
+            (List.length err)
+            pp_print_error err >>= fun () ->
+          exit 1
+    end
+
+let addr = ref Ipaddr.V6.localhost
+let port = ref (1024 + Random.int 8192)
+let clients = ref 10
+let repeat = ref 5
+
+let spec = Arg.[
+
+    "--port", Int (fun p -> port := p), " Listening port of the first peer.";
+
+    "--addr", String (fun p -> addr := Ipaddr.V6.of_string_exn p),
+    " Listening addr";
+
+    "--clients", Set_int clients,  " Number of concurrent clients." ;
+
+    "--repeat", Set_int repeat,  " Number of connections/disconnections." ;
+
+    "-v", Unit (fun () -> Lwt_log_core.(add_rule "p2p.connection-pool" Info)),
+    " Log up to info msgs" ;
+
+    "-vv", Unit (fun () -> Lwt_log_core.(add_rule "p2p.connection-pool" Debug)),
+    " Log up to debug msgs";
+
+  ]
+
+let main () =
+  let open Utils in
+  let anon_fun num_peers = raise (Arg.Bad "No anonymous argument.") in
+  let usage_msg = "Usage: %s <num_peers>.\nArguments are:" in
+  Arg.parse spec anon_fun usage_msg ;
+  let ports = !port -- (!port + !clients - 1) in
+  let points = List.map (fun port -> !addr, port) ports in
+  Lwt_list.iter_p (make_net points !repeat) (0 -- (!clients - 1))
+
+let () =
+  Sys.catch_break true ;
+  try
+    Logging.init Stderr ;
+    Lwt_main.run @@ main ()
+  with _ -> ()
