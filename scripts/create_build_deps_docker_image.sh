d96efa88c9e46b95dbeb5b2e9afef501dc58c611
Dynamic Ledger Solutions, Inc <contact@tezos.com>
2017-01-22 01:59:18 +0100

    Docker: integration in CI
    And minor fix:
    
    - introduce configuration file for OCaml and alpine versions
    - add missing `libssl1.0` in docker image
    - remove the split call to `install_build_deps` (not required anymore)
    - rename scripts

---
+++ scripts/create_build_deps_docker_image.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+set -e
+set -x
+
+script_dir="$(dirname "$(readlink -f "$0")")"
+src_dir="$(dirname "$script_dir")"
+cd "$src_dir"
+
+. scripts/version.sh
+image_name="${1:-tezos_build_deps}"
+image_version="${2:-latest}"
+
+sed scripts/Dockerfile.build_deps.in \
+    -e 's/$alpine_version/'"$alpine_version"'/g' \
+    -e 's/$ocaml_version/'"$ocaml_version"'/g' > Dockerfile
+
+docker build --pull -t "$image_name:$image_version" .
+
+rm Dockerfile
