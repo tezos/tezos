d61408d09c2756b46148dc8814d875f3b954aa7d
Dynamic Ledger Solutions, Inc <contact@tezos.com>
2017-02-14 12:24:56 +0100

    Client/Baker: improve logging.

--- src/client/embedded/bootstrap/baker/client_mining_revelation.ml
+++ src/client/embedded/bootstrap/baker/client_mining_revelation.ml
@@ -27,13 +27,19 @@ type Error_monad.error += Bad_revelation
 let forge_seed_nonce_revelation
     (cctxt: Client_commands.context)
     block ?(force = false) nonces =
+  Client_node_rpcs.Blocks.hash cctxt block >>= fun hash ->
   match nonces with
   | [] ->
-      cctxt.message "No nonce to reveal" >>= fun () ->
+      cctxt.message "No nonce to reveal for block %a"
+        Block_hash.pp_short hash >>= fun () ->
       return ()
   | _ ->
       inject_seed_nonce_revelation cctxt
         block ~force ~wait:true nonces >>=? fun oph ->
-      cctxt.answer "Operation successfully injected in the node." >>= fun () ->
-      cctxt.answer "Operation hash is '%a'." Operation_hash.pp_short oph >>= fun () ->
+      cctxt.answer
+        "Operation successfully injected %d revelation(s) for %a."
+        (List.length nonces)
+        Block_hash.pp_short hash >>= fun () ->
+      cctxt.answer "Operation hash is '%a'."
+        Operation_hash.pp_short oph >>= fun () ->
       return ()
