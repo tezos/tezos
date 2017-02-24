845ad80df22a8f691911be4144c8cd2bd6124edf
Dynamic Ledger Solutions, Inc <contact@tezos.com>
2017-02-24 17:10:19 +0100

    Makefile: praise the return of `utop`.
    The Makefile is really hackish...

--- src/utils/logging.ml
+++ src/utils/logging.ml
@@ -95,6 +95,7 @@ type template = Lwt_log.template
 let default_template = "$(date) - $(section): $(message)"
 
 module Output = struct
+
   type t =
     | Null
     | Stdout
@@ -102,69 +103,70 @@ module Output = struct
     | File of string
     | Syslog of Lwt_log.syslog_facility
 
+  let to_string : t -> string = function
+    | Null -> "/dev/null"
+    | Stdout -> "stdout"
+    | Stderr -> "stderr"
+    | File fp -> fp
+    | Syslog `Auth -> "syslog:auth"
+    | Syslog `Authpriv -> "syslog:authpriv"
+    | Syslog `Cron -> "syslog:cron"
+    | Syslog `Daemon -> "syslog:daemon"
+    | Syslog `FTP -> "syslog:ftp"
+    | Syslog `Kernel -> "syslog:kernel"
+    | Syslog `Local0 -> "syslog:local0"
+    | Syslog `Local1 -> "syslog:local1"
+    | Syslog `Local2 -> "syslog:local2"
+    | Syslog `Local3 -> "syslog:local3"
+    | Syslog `Local4 -> "syslog:local4"
+    | Syslog `Local5 -> "syslog:local5"
+    | Syslog `Local6 -> "syslog:local6"
+    | Syslog `Local7 -> "syslog:local7"
+    | Syslog `LPR -> "syslog:lpr"
+    | Syslog `Mail -> "syslog:mail"
+    | Syslog `News -> "syslog:news"
+    | Syslog `Syslog -> "syslog:syslog"
+    | Syslog `User -> "syslog:user"
+    | Syslog `UUCP -> "syslog:uucp"
+    | Syslog `NTP -> "syslog:ntp"
+    | Syslog `Security -> "syslog:security"
+    | Syslog `Console -> "syslog:console"
+
+  let of_string : string -> t = function
+    | "/dev/null" | "null" -> Null
+    | "stdout" -> Stdout
+    | "stderr" -> Stderr
+    | "syslog:auth" -> Syslog `Auth
+    | "syslog:authpriv" -> Syslog `Authpriv
+    | "syslog:cron" -> Syslog `Cron
+    | "syslog:daemon" -> Syslog `Daemon
+    | "syslog:ftp" -> Syslog `FTP
+    | "syslog:kernel" -> Syslog `Kernel
+    | "syslog:local0" -> Syslog `Local0
+    | "syslog:local1" -> Syslog `Local1
+    | "syslog:local2" -> Syslog `Local2
+    | "syslog:local3" -> Syslog `Local3
+    | "syslog:local4" -> Syslog `Local4
+    | "syslog:local5" -> Syslog `Local5
+    | "syslog:local6" -> Syslog `Local6
+    | "syslog:local7" -> Syslog `Local7
+    | "syslog:lpr" -> Syslog `LPR
+    | "syslog:mail" -> Syslog `Mail
+    | "syslog:news" -> Syslog `News
+    | "syslog:syslog" -> Syslog `Syslog
+    | "syslog:user" -> Syslog `User
+    | "syslog:uucp" -> Syslog `UUCP
+    | "syslog:ntp" -> Syslog `NTP
+    | "syslog:security" -> Syslog `Security
+    | "syslog:console" -> Syslog `Console
+    (* | s when start_with "syslog:" FIXME error or warning. *)
+    | fp ->
+        (* TODO check absolute path *)
+        File fp
+
   let encoding =
     let open Data_encoding in
-    conv
-      (function
-        | Null -> "/dev/null"
-        | Stdout -> "stdout"
-        | Stderr -> "stderr"
-        | File fp -> fp
-        | Syslog `Auth -> "syslog:auth"
-        | Syslog `Authpriv -> "syslog:authpriv"
-        | Syslog `Cron -> "syslog:cron"
-        | Syslog `Daemon -> "syslog:daemon"
-        | Syslog `FTP -> "syslog:ftp"
-        | Syslog `Kernel -> "syslog:kernel"
-        | Syslog `Local0 -> "syslog:local0"
-        | Syslog `Local1 -> "syslog:local1"
-        | Syslog `Local2 -> "syslog:local2"
-        | Syslog `Local3 -> "syslog:local3"
-        | Syslog `Local4 -> "syslog:local4"
-        | Syslog `Local5 -> "syslog:local5"
-        | Syslog `Local6 -> "syslog:local6"
-        | Syslog `Local7 -> "syslog:local7"
-        | Syslog `LPR -> "syslog:lpr"
-        | Syslog `Mail -> "syslog:mail"
-        | Syslog `News -> "syslog:news"
-        | Syslog `Syslog -> "syslog:syslog"
-        | Syslog `User -> "syslog:user"
-        | Syslog `UUCP -> "syslog:uucp"
-        | Syslog `NTP -> "syslog:ntp"
-        | Syslog `Security -> "syslog:security"
-        | Syslog `Console -> "syslog:console")
-      (function
-        | "/dev/null" | "null" -> Null
-        | "stdout" -> Stdout
-        | "stderr" -> Stderr
-        | "syslog:auth" -> Syslog `Auth
-        | "syslog:authpriv" -> Syslog `Authpriv
-        | "syslog:cron" -> Syslog `Cron
-        | "syslog:daemon" -> Syslog `Daemon
-        | "syslog:ftp" -> Syslog `FTP
-        | "syslog:kernel" -> Syslog `Kernel
-        | "syslog:local0" -> Syslog `Local0
-        | "syslog:local1" -> Syslog `Local1
-        | "syslog:local2" -> Syslog `Local2
-        | "syslog:local3" -> Syslog `Local3
-        | "syslog:local4" -> Syslog `Local4
-        | "syslog:local5" -> Syslog `Local5
-        | "syslog:local6" -> Syslog `Local6
-        | "syslog:local7" -> Syslog `Local7
-        | "syslog:lpr" -> Syslog `LPR
-        | "syslog:mail" -> Syslog `Mail
-        | "syslog:news" -> Syslog `News
-        | "syslog:syslog" -> Syslog `Syslog
-        | "syslog:user" -> Syslog `User
-        | "syslog:uucp" -> Syslog `UUCP
-        | "syslog:ntp" -> Syslog `NTP
-        | "syslog:security" -> Syslog `Security
-        | "syslog:console" -> Syslog `Console
-        (* | s when start_with "syslog:" FIXME error or warning. *)
-        | fp ->
-            (* TODO check absolute path *)
-            File fp)
-      string
+    conv to_string of_string string
 
   let of_string str =
     try
