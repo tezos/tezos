7d5b9aaedabaccb7f97c71b1df4e6d7a9359f727
Dynamic Ledger Solutions, Inc <contact@tezos.com>
2017-03-06 18:25:00 +0100

    RPC: add answer functions with HTTP code argument to RPC interface.

--- src/proto/environment/RPC.mli
+++ src/proto/environment/RPC.mli
@@ -98,7 +98,8 @@ module Answer : sig
   }
 
   val ok: 'a -> 'a answer
-  val return: 'a -> 'a answer Lwt.t
+  val answer: ?code:int -> 'a -> 'a answer
+  val return: ?code:int -> 'a -> 'a answer Lwt.t
 
 end
 
