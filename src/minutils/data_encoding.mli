1dceec8f5ea8fa24d2d535392caf52a94cb422f3
Dynamic Ledger Solutions, Inc <contact@tezos.com>
2017-04-13 18:05:41 +0200

    data_encoding: read streams of Mbytes.t, tests, remove dead code (debug functions), expose a function that checks without reading the data

--- src/minutils/data_encoding.mli
+++ src/minutils/data_encoding.mli
@@ -249,6 +249,30 @@ module Binary : sig
       MBytes.sub used internally *)
   val to_bytes_list : ?copy_blocks:bool -> int  -> 'a t -> 'a -> MBytes.t list
 
+  (** This type is used when decoding binary data incrementally.
+      - In case of 'Success', the decoded data, the size of used data
+       to decode the result, and the remaining data are returned
+      - In case of error, 'Error' is returned
+      - 'Await' status embeds a function that waits for additional data
+       to continue decoding, when given data are not sufficient *)
+  type 'a status =
+    | Success of { res : 'a ; res_len : int ; remaining : MBytes.t list }
+    | Await of (MBytes.t -> 'a status)
+    | Error
+
+  (** This function allows to decode (or to initialize decoding) a
+      stream of 'MByte.t'. The given data encoding should have a
+      'Fixed' or a 'Dynamic' size, otherwise an exception
+      'Invalid_argument "streaming data with variable size"' is
+      raised *)
+  val read_stream_of_bytes : ?init:MBytes.t list -> 'a t -> 'a status
+
+  (** Like read_stream_of_bytes, but only checks that the stream can
+      be read. Note that this is an approximation because failures
+      that may come from conversion functions present in encodings are
+      not checked *)
+  val check_stream_of_bytes : ?init:MBytes.t list -> 'a t -> unit status
+
   val fixed_length : 'a encoding -> int option
   val fixed_length_exn : 'a encoding -> int
 
