73bfe2ec4fb992b7896c38ed31f1df43873d6b4d
Dynamic Ledger Solutions, Inc <contact@tezos.com>
2017-04-19 15:32:27 +0200

    Docker: do not export the RPC port by default.

--- scripts/docker_entrypoint.inc.sh
+++ scripts/docker_entrypoint.inc.sh
@@ -33,16 +33,24 @@ init() {
         rmdir "$data_dir/bak"
         cp ~/scripts/alphanet_version "$data_dir/alphanet_version"
     fi
+    if [ "$#" -ge 2 ] && [ "$1" = "--rpc-port" ] ; then
+        rpc_addr="[::]:8732"
+        shift 2
+    else
+        rpc_addr="127.0.0.1:8732"
+    fi
     if [ ! -f "$node_dir/config.json" ]; then
         "$node" config init \
+                "$@" \
                 --data-dir "$node_dir" \
-                --rpc-addr "127.0.0.1:8732" \
-                --log-output "$node_dir/log" \
-                "$@"
+                --rpc-addr "$rpc_addr" \
+                --log-output "$node_dir/log"
     else
         "$node" config update \
+                "$@" \
                 --data-dir "$node_dir" \
-                "$@"
+                --rpc-addr "$rpc_addr" \
+                --log-output "$node_dir/log"
     fi
     if [ -f "$node_dir/identity.json" ]; then
         if ! "$node" identity check \
@@ -70,7 +78,7 @@ run_node() {
     ## Temporary hack until Pierre has debugged Lwt...
     export LWT_ASYNC_METHOD=none
     ## END of temporary hack
-    exec "$node" run --data-dir "$node_dir" "$@"
+    exec "$node" run "$@" --data-dir "$node_dir"
 }
 
 stop_node() {
