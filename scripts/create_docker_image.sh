6d255c8f77758516fd74c2a9fad09970632c4a5f
Dynamic Ledger Solutions, Inc <contact@tezos.com>
2017-08-04 18:32:52 +0200

    Update CI

--- scripts/create_docker_image.sh
+++ scripts/create_docker_image.sh
@@ -9,32 +9,14 @@ cd "$src_dir"
 . scripts/version.sh
 image_name="${1:-tezos}"
 image_version="${2:-latest}"
-build_deps_image_name="$image_name"_build_deps
+build_image_name="${image_name}_build"
+build_deps_image_name="${image_name}_build_deps"
 
-echo Building dependencies...
-./scripts/create_build_deps_docker_image.sh \
+./scripts/create_docker_image.build_deps.sh \
     "$build_deps_image_name" "$image_version"
 
-cleanup () {
-    set +e
-    echo Cleaning up...
-    [ -z "$tmp_container" ] || docker rm "$tmp_container"
-    [ -z "$tmp_image" ] || docker rmi "$tmp_image"
-    rm -rf Dockerfile bin
-}
-trap cleanup EXIT INT
+./scripts/create_docker_image.build.sh \
+    "$build_image_name" "$image_version" "$build_deps_image_name"
 
-sed scripts/Dockerfile.build_bin.in \
-    -e 's|$base_name|'"$build_deps_image_name"'|g' \
-    -e 's|$base_version|'"$image_version"'|g'  > Dockerfile
-
-echo Building tezos...
-tmp_image="$(docker build -q .)"
-tmp_container="$(docker run -dit "$tmp_image" true)"
-
-docker cp "$tmp_container":/home/opam/bin/ bin
-
-echo Building minimal docker image...
-sed scripts/Dockerfile.binaries.in \
-    -e 's|$alpine_version|'"$alpine_version"'|g' > Dockerfile
-docker build -q -t "$image_name:$image_version" .
+./scripts/create_docker_image.minimal.sh \
+    "$image_name" "$image_version" "$build_image_name"
