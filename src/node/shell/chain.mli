1d71ea5718e667a1215d86c851dc1666ab796443
Grégoire Henry <gregoire.henry@tezos.com>
2017-11-14 01:24:25 +0100

    Fix docstring attachment

--- src/node/shell/chain.mli
+++ src/node/shell/chain.mli
@@ -11,26 +11,26 @@
 
 open State
 
-val genesis: Net.t -> Block.t Lwt.t
 (** The genesis block of the network's blockchain. On a test network,
     the test protocol has been promoted as "main" protocol. *)
+val genesis: Net.t -> Block.t Lwt.t
 
-val head: Net.t -> Block.t Lwt.t
 (** The current head of the network's blockchain. *)
+val head: Net.t -> Block.t Lwt.t
 
-val known_heads: Net.t -> Block.t list Lwt.t
 (** The current head and all the known (valid) alternate heads. *)
+val known_heads: Net.t -> Block.t list Lwt.t
 
-val mem: Net.t -> Block_hash.t -> bool Lwt.t
 (** Test whether a block belongs to the current mainnet. *)
+val mem: Net.t -> Block_hash.t -> bool Lwt.t
 
-val set_head: Net.t -> Block.t -> Block.t Lwt.t
 (** Record a block as the current head of the network's blockchain.
     It returns the previous head. *)
+val set_head: Net.t -> Block.t -> Block.t Lwt.t
 
-val test_and_set_head:
-  Net.t -> old:Block.t -> Block.t -> bool Lwt.t
 (** Atomically change the current head of the network's blockchain.
     This returns [true] whenever the change succeeded, or [false]
     when the current head os not equal to the [old] argument. *)
+val test_and_set_head:
+  Net.t -> old:Block.t -> Block.t -> bool Lwt.t
 
