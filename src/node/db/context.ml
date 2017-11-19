485d8bc26acdd98304b10720f4f446c7f7dd1899
Grégoire Henry <gregoire.henry@tezos.com>
2017-11-19 21:25:28 +0100

    Context: quick-and-dirty leveldb backend for Irmin
    This is a dirty and non-optimized backend, it is still faster than the
    current git backend.
    
    Main drawbacks:
    - the leveldb binding is non-coopertive, the node will
      block while committing a block to the disk ;
    - the leveldb use 'string' while internally we use 'cstruct',
      this implies a lot of time-consuming 'memcpy'.

--- src/node/db/context.ml
+++ src/node/db/context.ml
@@ -24,16 +24,25 @@ module MBytesContent = struct
   let of_string s = Ok (MBytes.of_string s)
 end
 
+module Metadata = struct
+  type t = unit
+  let t = Irmin.Type.unit
+  let default = ()
+  let merge = Irmin.Merge.default t
+end
+
 module GitStore =
-  Irmin_unix.Git.FS.Make
-    (MBytesContent) (Irmin.Path.String_list) (Irmin.Branch.String)
+  Irmin_leveldb.Make
+    (Metadata)
+    (MBytesContent)
+    (Irmin.Path.String_list)
+    (Irmin.Branch.String)
+    (Irmin.Hash.SHA1)
 
 type index = {
   path: string ;
   repo: GitStore.Repo.t ;
   patch_context: context -> context Lwt.t ;
-  mutable commits: int ;
-  repack_scheduler: Lwt_utils.Idle_waiter.t ;
 }
 
 and context = {
@@ -73,13 +82,11 @@ let current_protocol_key = ["protocol"]
 let current_test_network_key = ["test_network"]
 
 let exists index key =
-  Lwt_utils.Idle_waiter.task index.repack_scheduler @@ fun () ->
   GitStore.Commit.of_hash index.repo key >>= function
   | None -> Lwt.return_false
   | Some _ -> Lwt.return_true
 
 let checkout index key =
-  Lwt_utils.Idle_waiter.task index.repack_scheduler @@ fun () ->
   GitStore.Commit.of_hash index.repo key >>= function
   | None -> Lwt.return_none
   | Some commit ->
@@ -101,37 +108,8 @@ let raw_commit ~time ~message context =
     context.index.repo ~info ~parents:context.parents context.tree
 
 let commit ~time ~message context =
-  begin
-    Lwt_utils.Idle_waiter.task context.index.repack_scheduler @@ fun () ->
-    raw_commit ~time ~message context >>= fun commit ->
-    Lwt.return (GitStore.Commit.hash commit)
-  end >>= fun commit ->
-  context.index.commits <- context.index.commits + 1 ;
-  if context.index.commits mod 200 <> 0 then Lwt.return commit
-  else begin
-    Lwt_utils.Idle_waiter.force_idle
-      context.index.repack_scheduler
-      begin fun () ->
-        let open Logging.Db in
-        lwt_debug "begin git repack" >>= fun () ->
-        let command =
-          "git",
-          [| "git" ; "-C" ; context.index.path ;
-             "repack" ; "-a" ; "-d" |] in
-        let t0 = Unix.gettimeofday () in
-        Lwt_process.exec
-          ~stdout: `Dev_null ~stderr: `Dev_null
-          command >>= fun res ->
-        let dt = Unix.gettimeofday () -. t0 in
-        match res with
-        | WEXITED 0 ->
-            lwt_log_notice "git repack complete in %0.2f sec" dt
-        | WEXITED code | WSTOPPED code | WSIGNALED code ->
-            lwt_log_error "git repack failed with code %d after %0.2f sec"
-              code dt
-      end
-  end >>= fun () ->
-    Lwt.return commit
+  raw_commit ~time ~message context >>= fun commit ->
+  Lwt.return (GitStore.Commit.hash commit)
 
 (*-- Generic Store Primitives ------------------------------------------------*)
 
@@ -144,39 +122,32 @@ type key = string list
 type value = MBytes.t
 
 let mem ctxt key =
-  Lwt_utils.Idle_waiter.task ctxt.index.repack_scheduler @@ fun () ->
   GitStore.Tree.mem ctxt.tree (data_key key) >>= fun v ->
   Lwt.return v
 
 let dir_mem ctxt key =
-  Lwt_utils.Idle_waiter.task ctxt.index.repack_scheduler @@ fun () ->
   GitStore.Tree.mem_tree ctxt.tree (data_key key) >>= fun v ->
   Lwt.return v
 
 let raw_get ctxt key =
-  Lwt_utils.Idle_waiter.task ctxt.index.repack_scheduler @@ fun () ->
   GitStore.Tree.find ctxt.tree key
 let get t key = raw_get t (data_key key)
 
 let raw_set ctxt key data =
-  Lwt_utils.Idle_waiter.task ctxt.index.repack_scheduler @@ fun () ->
   GitStore.Tree.add ctxt.tree key data >>= fun tree ->
   Lwt.return { ctxt with tree }
 let set t key data = raw_set t (data_key key) data
 
 let raw_del ctxt key =
-  Lwt_utils.Idle_waiter.task ctxt.index.repack_scheduler @@ fun () ->
   GitStore.Tree.remove ctxt.tree key >>= fun tree ->
   Lwt.return { ctxt with tree }
 let del t key = raw_del t (data_key key)
 
 let remove_rec ctxt key =
-  Lwt_utils.Idle_waiter.task ctxt.index.repack_scheduler @@ fun () ->
   GitStore.Tree.remove ctxt.tree (data_key key) >>= fun tree ->
   Lwt.return { ctxt with tree }
 
 let fold ctxt key ~init ~f =
-  Lwt_utils.Idle_waiter.task ctxt.index.repack_scheduler @@ fun () ->
   GitStore.Tree.list ctxt.tree (data_key key) >>= fun keys ->
   Lwt_list.fold_left_s
     begin fun acc (name, kind) ->
@@ -274,12 +245,10 @@ let fork_test_network v ~protocol ~expiration =
 
 let init ?patch_context ~root =
   GitStore.Repo.v
-    (Irmin_git.config ~bare:true root) >>= fun repo ->
+    (Irmin_leveldb.config root) >>= fun repo ->
   Lwt.return {
-    commits = 0 ;
     path = root ;
     repo ;
-    repack_scheduler = Lwt_utils.Idle_waiter.create () ;
     patch_context =
       match patch_context with
       | None -> (fun ctxt -> Lwt.return ctxt)
@@ -290,7 +259,6 @@ let get_branch net_id = Format.asprintf "%a" Net_id.pp net_id
 
 
 let commit_genesis index ~net_id ~time ~protocol =
-  Lwt_utils.Idle_waiter.task index.repack_scheduler @@ fun () ->
   let tree = GitStore.Tree.empty in
   let ctxt = { index ; tree ; parents = [] } in
   index.patch_context ctxt >>= fun ctxt ->
@@ -306,7 +274,6 @@ let compute_testnet_genesis forked_block =
   net_id, genesis
 
 let commit_test_network_genesis index forked_block time ctxt =
-  Lwt_utils.Idle_waiter.task index.repack_scheduler @@ fun () ->
   let net_id, genesis = compute_testnet_genesis forked_block in
   let branch = get_branch net_id in
   let message = Format.asprintf "Forking testnet: %s." branch in
@@ -330,12 +297,10 @@ let reset_test_network ctxt forked_block timestamp =
 
 let clear_test_network index net_id =
   (* TODO remove commits... ??? *)
-  Lwt_utils.Idle_waiter.task index.repack_scheduler @@ fun () ->
   let branch = get_branch net_id in
   GitStore.Branch.remove index.repo branch
 
 let set_head index net_id commit =
-  Lwt_utils.Idle_waiter.task index.repack_scheduler @@ fun () ->
   let branch = get_branch net_id in
   GitStore.Commit.of_hash index.repo commit >>= function
   | None -> assert false
@@ -343,7 +308,6 @@ let set_head index net_id commit =
       GitStore.Branch.set index.repo branch commit
 
 let set_master index commit =
-  Lwt_utils.Idle_waiter.task index.repack_scheduler @@ fun () ->
   GitStore.Commit.of_hash index.repo commit >>= function
   | None -> assert false
   | Some commit ->
