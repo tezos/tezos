054506a85f5b347813d21de779947a305a176277
Milo Davis <davis.mil@husky.neu.edu>
2017-12-14 11:33:38 +0100

    Init-client: fix bash-completion error

--- bin_client/tezos-init-sandboxed-client.sh
+++ bin_client/tezos-init-sandboxed-client.sh
@@ -241,7 +241,7 @@ if type tezos-client-reset >/dev/null 2>&1 ; then tezos-client-reset; fi ;
 alias tezos-client="$client" ;
 alias tezos-activate-alpha="$client -block genesis activate protocol ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK with fitness 1 and passes 1 and key dictator" ;
 alias tezos-client-reset="rm -rf \"$client_dir\"; unalias tezos-client tezos-activate-alpha tezos-client-reset" ;
-alias tezos-autocomplete="source \"$script_dir/bash-completion.sh\"" ;
+alias tezos-autocomplete="source \"$bin_dir/bash-completion.sh\"" ;
 trap tezos-client-reset EXIT ;
 EOF
 
