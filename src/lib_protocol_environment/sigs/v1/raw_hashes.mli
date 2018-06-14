5fe6bd9a548e6c81aad01e3aae6c223c26713001
Benjamin Canou <benjamin@canou.fr>
2018-06-14 18:58:25 +0200

    Proto: add raw_hashes.mli to introduces new hashes in Michelson

---
+++ src/lib_protocol_environment/sigs/v1/raw_hashes.mli
@@ -0,0 +1,14 @@
+(**************************************************************************)
+(*                                                                        *)
+(*    Copyright (c) 2014 - 2018.                                          *)
+(*    Dynamic Ledger Solutions, Inc. <contact@tezos.com>                  *)
+(*                                                                        *)
+(*    All rights reserved. No warranty, explicit or implicit, provided.   *)
+(*                                                                        *)
+(**************************************************************************)
+
+val blake2b : MBytes.t -> MBytes.t
+
+val sha256 : MBytes.t -> MBytes.t
+
+val sha512 : MBytes.t -> MBytes.t
