dd8e4d85f9565c4c757e66e889e942a21774527f
Pierre Chambart <pierre.chambart@ocamlpro.com>
2018-06-22 00:03:11 +0200

    Alpha: Rename Fees into Fees_storage
    It was moved before ALpha_context

--- src/proto_alpha/lib_protocol/src/alpha_context.ml
+++ src/proto_alpha/lib_protocol/src/alpha_context.ml
@@ -54,7 +54,7 @@ module Script = struct
   include Michelson_v1_primitives
   include Script_repr
 end
-module Fees = Fees
+module Fees = Fees_storage
 
 type public_key = Signature.Public_key.t
 type public_key_hash = Signature.Public_key_hash.t
