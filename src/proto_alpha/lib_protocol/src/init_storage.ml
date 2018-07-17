15857d7e90e45f4e54cd805d8fd2d8b4d05cccc4
Pierre Chambart <pierre.chambart@ocamlpro.com>
2018-07-17 18:07:43 +0200

    Alpha_002: prepare for `Proto_alpha` upgrade

--- src/proto_alpha/lib_protocol/src/init_storage.ml
+++ src/proto_alpha/lib_protocol/src/init_storage.ml
@@ -26,21 +26,25 @@
 (* This is the genesis protocol: initialise the state *)
 let prepare_first_block ctxt ~typecheck ~level ~timestamp ~fitness =
   Raw_context.prepare_first_block
-    ~level ~timestamp ~fitness ctxt >>=? fun (param, ctxt) ->
-  Commitment_storage.init ctxt param.commitments >>=? fun ctxt ->
-  Roll_storage.init ctxt >>=? fun ctxt ->
-  Seed_storage.init ctxt >>=? fun ctxt ->
-  Contract_storage.init ctxt >>=? fun ctxt ->
-  Bootstrap_storage.init ctxt
-    ~typecheck
-    ?ramp_up_cycles:param.security_deposit_ramp_up_cycles
-    ?no_reward_cycles:param.no_reward_cycles
-    param.bootstrap_accounts
-    param.bootstrap_contracts >>=? fun ctxt ->
-  Roll_storage.init_first_cycles ctxt >>=? fun ctxt ->
-  Vote_storage.init ctxt >>=? fun ctxt ->
-  Storage.Last_block_priority.init ctxt 0 >>=? fun ctxt ->
-  return ctxt
+    ~level ~timestamp ~fitness ctxt >>=? fun (previous_protocol, ctxt) ->
+  match previous_protocol with
+  | Genesis param ->
+      Commitment_storage.init ctxt param.commitments >>=? fun ctxt ->
+      Roll_storage.init ctxt >>=? fun ctxt ->
+      Seed_storage.init ctxt >>=? fun ctxt ->
+      Contract_storage.init ctxt >>=? fun ctxt ->
+      Bootstrap_storage.init ctxt
+        ~typecheck
+        ?ramp_up_cycles:param.security_deposit_ramp_up_cycles
+        ?no_reward_cycles:param.no_reward_cycles
+        param.bootstrap_accounts
+        param.bootstrap_contracts >>=? fun ctxt ->
+      Roll_storage.init_first_cycles ctxt >>=? fun ctxt ->
+      Vote_storage.init ctxt >>=? fun ctxt ->
+      Storage.Last_block_priority.init ctxt 0 >>=? fun ctxt ->
+      return ctxt
+  | Alpha ->
+      return ctxt
 
 let prepare ctxt ~level ~timestamp ~fitness =
   Raw_context.prepare ~level ~timestamp ~fitness ctxt
