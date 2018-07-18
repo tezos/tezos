86091e98c6898f2e84382a9f436dff48ed5a205b
Grégoire Henry <gregoire.henry@tezos.com>
2018-07-18 20:21:23 +0200

    Baker: use `next_protocols` when calling `GET /monitor/heads`

--- src/proto_alpha/lib_delegate/client_daemon.ml
+++ src/proto_alpha/lib_delegate/client_daemon.ml
@@ -27,6 +27,7 @@ module Endorser = struct
 
   let run (cctxt : #Proto_alpha.full) ~delay ?min_date delegates =
     Client_baking_blocks.monitor_heads
+      ~next_protocols:(Some [Proto_alpha.hash])
       cctxt `Main >>=? fun block_stream ->
     Client_baking_endorsement.create cctxt ~delay delegates block_stream >>=? fun () ->
     ignore min_date;
@@ -38,6 +39,7 @@ module Baker = struct
 
   let run (cctxt : #Proto_alpha.full) ?threshold ?max_priority ?min_date ~context_path delegates =
     Client_baking_blocks.monitor_heads
+      ~next_protocols:(Some [Proto_alpha.hash])
       cctxt `Main >>=? fun block_stream ->
     Client_baking_forge.create cctxt
       ?threshold ?max_priority ~context_path delegates block_stream >>=? fun () ->
