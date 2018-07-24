e85bdaeb14dd8cdac09b7a7bc807c4d475c6e327
Grégoire Henry <gregoire.henry@tezos.com>
2018-07-24 20:29:08 +0200

    Opam: update dependencies

--- scripts/opam-check.sh
+++ scripts/opam-check.sh
@@ -5,26 +5,33 @@ src_dir="$(dirname "$script_dir")"
 
 . "$script_dir"/version.sh
 
-. "$script_dir"/opam-pin.sh
+opams=$(find "$src_dir/vendors" "$src_dir/src" -name \*.opam -print)
 
 echo "## Checking installed dependencies..."
 echo
 
-if ! opam install $packages --deps-only --with-test --show-actions | grep "Nothing to do." > /dev/null 2>&1 ; then
+if ! opam install $opams --deps-only --with-test --show-actions | grep "Nothing to do." > /dev/null 2>&1 ; then
     echo
     echo 'Failure! Missing actions:'
     echo
     opam install $packages --deps-only --with-test --show-actions
     echo
-    echo 'Running `./scripts/update_opam_repo.sh`'
+    echo 'Failed! Please read the doc in `./scripts/update_opam_repo.sh` and act accordingly.'
     echo
-    ./scripts/update_opam_repo.sh
-    echo
-    echo 'Result:'
-    echo
+    exit 1
+fi
+
+echo '## Running `./scripts/update_opam_repo.sh`'
+echo
+./scripts/update_opam_repo.sh
+
+if [ -n "$(cat opam_repo.patch)" ] ; then
+
+    echo "##################################################"
     cat opam_repo.patch
-    echo
-    echo 'Failed! Please run: `./scripts/update_opam_repo.sh` and follow the instructions.'
+    echo "##################################################"
+
+    echo 'Failed! The variables `opam_repository_tag` and `full_opam_repository_tag` are not synchronized. Please read the doc in `./scripts/update_opam_repo.sh` and act accordingly.'
     echo
     exit 1
 fi
