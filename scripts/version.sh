e6ed10bb12bc3ed59fb43410d728832bf099480b
Grégoire Henry <gregoire.henry@tezos.com>
2018-07-24 20:29:08 +0200

    Opam: update dependencies

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,9 +4,10 @@
 ## `lib.protocol-compiler/tezos-protocol-compiler.opam`
 
 ocaml_version=4.06.1
-opam_version=2.0.0~rc3
+opam_version=2.0.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=63f06ab145e22aef4cc55b5b9557756d4c868843
+opam_repository_tag=4200cbebcae75562d79898a8e11e9f4dc237cc08
+full_opam_repository_tag=2274027270cc192b88ecd7b9d4035dba66b2cffd
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
