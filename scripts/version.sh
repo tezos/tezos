0a4178934e98299e862eba282048cba1bf40b754
Eddie Wang <eddiew@Eddies-MacBook-Pro.local>
2018-07-30 15:49:15 -0400

    bump opam version to 2.0.0

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,8 +6,8 @@
 alpine_version=3.6
 ocaml_version=4.06.1
 hidapi_version=0.8.0_rc1
-opam_version=2.0.0~rc3
-opam_tag=2.0.0-rc3
+opam_version=2.0.0
+opam_tag=2.0.0
 opam_repository_tag=7db0db5dc53de44d1a0190b1310aa225e04df9ef
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
