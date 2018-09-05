866b24453fe5488448ae66f633bdc916c1292005
Grégoire Henry <gregoire.henry@tezos.com>
2018-09-05 18:48:07 +0200

    Shell/RPC: export a Base58Check representation of block headers
    Co-authored-by: Benjamin Canou <benjamin@canou.fr>
    Co-authored-by: Pierre Boutillier <pierre.boutillier@ens-lyon.org>

--- src/lib_base/block_header.ml
+++ src/lib_base/block_header.ml
@@ -106,6 +106,14 @@ let to_bytes v = Data_encoding.Binary.to_bytes_exn encoding v
 let of_bytes b = Data_encoding.Binary.of_bytes encoding b
 let of_bytes_exn b = Data_encoding.Binary.of_bytes_exn encoding b
 
+let to_b58check v =
+  Base58.safe_encode (MBytes.to_string (to_bytes v))
+
+let of_b58check b =
+  Option.apply (Base58.safe_decode b) ~f: begin fun s ->
+    Data_encoding.Binary.of_bytes encoding (MBytes.of_string s)
+  end
+
 let hash block = Block_hash.hash_bytes [to_bytes block]
 let hash_raw bytes = Block_hash.hash_bytes [bytes]
 
