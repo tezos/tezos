0feb5bd07aacfa424863bc7cb0edd927fb25a7ee
Benjamin Canou <benjamin@canou.fr>
2018-10-04 17:12:27 +0200

    Signer: fix animals test

--- src/lib_signer_backends/test/test_crouching.ml
+++ src/lib_signer_backends/test/test_crouching.ml
@@ -1,7 +1,7 @@
 
 let test_example () =
   let name = Ledger_names.crouching_tiger "12345" in
-  assert (name = "calculating-meerkat-straight-beetle")
+  assert (name = { c = "calculating" ; t = "meerkat" ; h = "straight" ; d = "beetle" })
 
 let tests = [
   Alcotest.test_case "print_example" `Quick test_example;
