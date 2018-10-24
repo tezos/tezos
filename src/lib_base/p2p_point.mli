be5f985ec7ba25006e2cee5505e5f3e13af44c94
Julien Tesson <julien.tesson@tezcore.com>
2018-10-24 17:06:16 +0200

    P2p: exporting `Point.Id.equal`

--- src/lib_base/p2p_point.mli
+++ src/lib_base/p2p_point.mli
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2019 Nomadic Labs, <contact@nomadic-labs.com>               *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -27,6 +28,7 @@ module Id : sig
 
   type t = P2p_addr.t * P2p_addr.port
   val compare : t -> t -> int
+  val equal :  t -> t -> bool
 
   val pp : Format.formatter -> t -> unit
   val pp_opt : Format.formatter -> t option -> unit
