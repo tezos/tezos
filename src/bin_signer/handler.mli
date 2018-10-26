f898062fd4a92e1c5d35990bd9aa4683d60c2fa7
Vincent Bernardoff <vb@luminar.eu.org>
2018-10-26 16:51:11 +0800

    Signer: add `handler.mli`

---
+++ src/bin_signer/handler.mli
@@ -0,0 +1,42 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+module Authorized_key :
+  Client_aliases.Alias with type t := Signature.public_key
+(** Storage for keys that have been authorized for baking. *)
+
+val public_key :
+  #Client_context.wallet ->
+  Signature.public_key_hash -> Signature.public_key tzresult Lwt.t
+(** [public_key cctxt pkh] returns the public key whose hash is [pkh]
+    iff it is present if [cctxt]. *)
+
+val sign :
+  #Client_context.wallet ->
+  Signer_messages.Sign.Request.t ->
+  ?magic_bytes:int list ->
+  check_high_watermark:bool -> require_auth:bool -> Signature.t tzresult Lwt.t
+(** [sign cctxt req ?magic_bytes ~check_high_watermark ~require_auth]
+    signs [req] and returns a signature. *)
