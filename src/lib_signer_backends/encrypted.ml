dc67e939079495cc284862599820e925c5c47b81
MBourgoin <mathias.bourgoin@tezcore.com>
2019-01-22 15:23:18 +0100

    Client: update copyrights

--- src/lib_signer_backends/encrypted.ml
+++ src/lib_signer_backends/encrypted.ml
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2018 Nomadic Labs, <contact@nomadic-labs.com>               *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
