14656a288b8f7407985786a3ebf29e5027902f73
Grégoire Henry <gregoire.henry@tezos.com>
2019-02-19 17:08:02 +0100

    Make: add `make build-dev-deps`

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -5,6 +5,12 @@ src_dir="$(dirname "$script_dir")"
 
 . "$script_dir"/version.sh
 
+if [ "$1" = "--dev" ]; then
+    dev=yes
+else
+    dev=
+fi
+
 opam repository set-url tezos --dont-select $opam_repository || \
     opam repository add tezos --dont-select $opam_repository > /dev/null 2>&1
 
@@ -21,6 +27,10 @@ fi
 
 eval $(opam env --shell=sh)
 
+if [ -n "$dev" ]; then
+    opam repository remove default > /dev/null 2>&1 || true
+fi
+
 if [ "$(ocaml -vnum)" != "$ocaml_version" ]; then
     opam install --unlock-base ocaml-base-compiler.$ocaml_version
 fi
@@ -28,3 +38,8 @@ fi
 opam list --installed opam-depext || opam --yes install opam-depext
 
 "$script_dir"/install_build_deps.raw.sh
+
+if [ -n "$dev" ]; then
+    opam repository add default --rank=-1 > /dev/null 2>&1 || true
+    opam install merlin
+fi
