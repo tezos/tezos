38f5424c3d130cbd88d7c58484c454e1105c6ee3
Victor Allombert <victor.allombert@tezcore.com>
2019-02-27 09:04:50 +0100

    lib_storage: Add shallow clonning with metadata
    Co-authored-by: Thomas Blanc <thomas.blanc@ens-cachan.fr>
    Co-authored-by: Pierre Chambart <pierre.chambart@ocamlpro.com>
    Co-authored-by: Benjamin Canou <benjamin@canou.fr>

--- src/lib_storage/context.mli
+++ src/lib_storage/context.mli
@@ -62,6 +62,8 @@ val commit_test_chain_genesis:
 (** {2 Generic interface} ****************************************************)
 
 type key = string list
+(** [key] indicates a path in a context. *)
+
 type value = MBytes.t
 
 val mem: context -> key -> bool Lwt.t
@@ -74,6 +76,8 @@ val remove_rec: context -> key -> t Lwt.t
 (** [copy] returns None if the [from] key is not bound *)
 val copy: context -> from:key -> to_:key -> context option Lwt.t
 
+(** [fold] iterates over elements under a path (not recursive). Iteration order
+    is undeterministic. *)
 val fold:
   context -> key -> init:'a ->
   f:([ `Key of key | `Dir of key ] -> 'a -> 'a Lwt.t) ->
@@ -108,3 +112,38 @@ val del_test_chain: context -> context Lwt.t
 val fork_test_chain:
   context -> protocol:Protocol_hash.t -> expiration:Time.t -> context Lwt.t
 val clear_test_chain: index -> Chain_id.t -> unit Lwt.t
+
+(** {2 Context dumping} ******************************************************)
+
+module Pruned_block : sig
+
+  type t = {
+    block_header : Block_header.t ;
+    operations : ( int * Operation.t list ) list ;
+    operation_hashes : (int * Operation_hash.t list) list ;
+  }
+
+  val encoding : t Data_encoding.t
+
+  val to_bytes : t -> MBytes.t
+  val of_bytes : MBytes.t -> t option
+end
+
+module Block_data : sig
+
+  type t = {
+    block_header : Block_header.t ;
+    operations : Operation.t list list ;
+  }
+
+  val to_bytes : t -> MBytes.t
+  val of_bytes : MBytes.t -> t option
+  val empty : t
+  val encoding : t Data_encoding.t
+end
+
+val dump_contexts : index -> (Block_header.t * Block_data.t * Pruned_block.t list) list -> filename:string ->
+  unit tzresult Lwt.t
+
+val dump_contexts_fd : index -> (Block_header.t * Block_data.t * Pruned_block.t list) list -> fd:Lwt_unix.file_descr ->
+  unit tzresult Lwt.t
