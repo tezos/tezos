6cb9a29dcc93c31fc146f5e4f1f0d7ab299e32c9
Vincent Botbol <vincent.botbol@tezcore.com>
2019-02-27 12:08:31 +0100

    Daemon: remove the files upgrade from the daemons

--- src/proto_003_PsddFKi3/lib_delegate/client_daemon.ml
+++ src/proto_003_PsddFKi3/lib_delegate/client_daemon.ml
@@ -102,7 +102,6 @@ let monitor_fork_testchain (cctxt: #Proto_alpha.full) ~cleanup_nonces  =
 module Endorser = struct
 
   let run (cctxt : #Proto_alpha.full) ~chain ~delay delegates =
-    Daemon_state.upgrade_files cctxt () >>=? fun () ->
     await_bootstrapped_node cctxt >>=? fun _ ->
     begin if chain = `Test then
         monitor_fork_testchain cctxt ~cleanup_nonces:false
@@ -132,7 +131,6 @@ module Baker = struct
       ~chain
       ~context_path
       delegates =
-    Daemon_state.upgrade_files cctxt () >>=? fun () ->
     await_bootstrapped_node cctxt >>=? fun _ ->
     begin if chain = `Test then
         monitor_fork_testchain cctxt ~cleanup_nonces:true
