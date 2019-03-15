07d413bf221ff45a9e51334bb6b50d9b880c9105
Philippe Wang <philippe.wang@gmail.com>
2019-03-15 15:28:06 -0700

    add some code documentation
    	modified:   src/lib_client_base_unix/client_main_run.mli

--- src/lib_client_base_unix/client_main_run.mli
+++ src/lib_client_base_unix/client_main_run.mli
@@ -31,24 +31,41 @@ module type M =
 sig
   type t
   val global_options :
+    (* Global options for the CLI. The presence of (unit ->) is
+       because of weak type variables. *)
     unit -> (t, Client_context_unix.unix_full) Clic.options
   val parse_config_args :
+    (* How to parse CLI arguments *)
     #Tezos_client_base.Client_context.full ->
     string list ->
     (Client_config.parsed_config_args * string list) tzresult Lwt.t
   val default_block : [> `Head of int ]
-  val default_base_dir : string
+  val default_base_dir :
+    (* You may use the default base directory in [Client_config] or
+       define your own one. *)
+    string
   val other_registrations :
+    (* You may give an **optional** function that will work on the
+       configuration file and the remote parameters. *)
     (Client_config.Cfg_file.t -> (module Client_config.Remote_params) -> unit)
       option
   val clic_commands :
+    (* This function defines how you put together different types of
+       commands. Default (in [Client_config]) is to simply append the lists
+       together. Arguments [base_dir] and [require_auth] are to be used
+       if you need them, default (in [Client_config]) is to ignore them. *)
     base_dir:string ->
     config_commands:Tezos_client_base.Client_context.full Clic.command list ->
     builtin_commands:Tezos_client_base.Client_context.full Clic.command list ->
     other_commands:Tezos_client_base.Client_context.full Clic.command list ->
     require_auth:bool ->
     Tezos_client_base.Client_context.full Clic.command list
-  val logger : RPC_client.logger option
+  val logger :
+    (* Provide your own [logger] here if you need to override the
+       logger that might come from elsewhere. Default (in [Client_config]) is
+       [None], but [Main_signer] uses this overriding feature. *)
+    RPC_client.logger option
+
 end
 
 val run :
