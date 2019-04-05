e7e3b4b93384a0690b94ed3958d3ba3c1d3fd68c
Victor Allombert <victor.allombert@tezcore.com>
2019-04-05 09:48:47 +0200

    bin_node: update upgrade command.

--- src/bin_node/node_upgrade_command.ml
+++ src/bin_node/node_upgrade_command.ml
@@ -29,37 +29,54 @@ let (//) = Filename.concat
 
 module Term = struct
 
-  let process data_dir config_file =
-    let run =
-      begin
-        match data_dir with
-        | Some data_dir -> return data_dir
-        | None ->
-            match config_file with
+  type upgrade = Storage
+
+  let subcommand_arg =
+    let parser = function
+      | "storage" -> `Ok Storage
+      | s -> `Error ("invalid argument: "^s)
+    and printer ppf = function
+      | Storage -> Format.fprintf ppf "storage"
+    in
+    let open Cmdliner.Arg in
+    let doc =
+      "Upgrade to perform. \
+       Possible values: $(b, storage)." in
+    required & pos 0 (some (parser, printer)) None & info [] ~docv:"UPGRADE" ~doc
+
+  let process subcommand data_dir config_file =
+    match subcommand with
+    | Storage ->
+        let run =
+          begin
+            match data_dir with
+            | Some data_dir -> return data_dir
             | None ->
-                let default_config =
-                  Node_config_file.default_data_dir // "config.json" in
-                if Sys.file_exists default_config then
-                  Node_config_file.read default_config >>=? fun cfg ->
-                  return cfg.data_dir
-                else
-                  return Node_config_file.default_data_dir
-            | Some config_file ->
-                Node_config_file.read config_file >>=? fun cfg ->
-                return cfg.data_dir
-      end >>=? fun data_dir ->
-      begin
-        trace (failure "Fail to lock the data directory. Is a `tezos-node` running?") @@
-        Lwt_lock_file.create
-          ~unlink_on_exit:true (Node_data_version.lock_file data_dir)
-      end >>=? fun () ->
-      Node_data_version.upgrade_data_dir data_dir in
-    match Lwt_main.run run with
-    | Ok () -> `Ok ()
-    | Error err -> `Error (false, Format.asprintf "%a" pp_print_error err)
+                match config_file with
+                | None ->
+                    let default_config =
+                      Node_config_file.default_data_dir // "config.json" in
+                    if Sys.file_exists default_config then
+                      Node_config_file.read default_config >>=? fun cfg ->
+                      return cfg.data_dir
+                    else
+                      return Node_config_file.default_data_dir
+                | Some config_file ->
+                    Node_config_file.read config_file >>=? fun cfg ->
+                    return cfg.data_dir
+          end >>=? fun data_dir ->
+          begin
+            trace (failure "Fail to lock the data directory. Is a `tezos-node` running?") @@
+            Lwt_lock_file.create
+              ~unlink_on_exit:true (Node_data_version.lock_file data_dir)
+          end >>=? fun () ->
+          Node_data_version.upgrade_data_dir data_dir in
+        match Lwt_main.run run with
+        | Ok () -> `Ok ()
+        | Error err -> `Error (false, Format.asprintf "%a" pp_print_error err)
 
   let term =
-    Cmdliner.Term.(ret (const process
+    Cmdliner.Term.(ret (const process $ subcommand_arg
                         $ Node_shared_arg.Term.data_dir
                         $ Node_shared_arg.Term.config_file))
 
@@ -68,12 +85,13 @@ end
 module Manpage = struct
 
   let command_description =
-    "The $(b,upgrade_storage) command is meant to manage upgrades \
-     in the node disk storage."
+    "The $(b,upgrade) command is meant to manage upgrades of the node."
 
   let description = [
     `S "DESCRIPTION" ;
     `P command_description ;
+    `P "Available ugrades are:";
+    `P "$(b,storage) will upgrade the node disk storage (if needed)."
   ]
 
   let man =
@@ -83,12 +101,11 @@ module Manpage = struct
 
   let info =
     Cmdliner.Term.info
-      ~doc: "Upgrade of the node disk storage if needed"
+      ~doc: "Upgrade of the node"
       ~man
-      "upgrade_storage"
+      "upgrade"
 
 end
 
 let cmd =
   Term.term, Manpage.info
-
