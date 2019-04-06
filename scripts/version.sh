cff908b5e6d2402fee60f00bfa3a3f27dd5d7212
Pietro Abate <pietro.abate@tezcore.com>
2019-04-06 11:45:52 +0200

    Opam repository: put git final git hashes
    Signed-off-by: Pierre Boutillier <pierre.boutillier@ens-lyon.org>

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=3229a77515ebbfc848b9ae87a8e8e420bb29873a
-full_opam_repository_tag=bf6159bb0d90eb976dbae89db9e2789dc29210cc
+opam_repository_tag=ebc2de016e52e8b77f250df174512a793b2fb0e5
+full_opam_repository_tag=2952acf6c0736562d1a94c3fd28ff01a8c38d56b
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
