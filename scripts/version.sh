d272059bf474018d0c39f5a6e60634a95f0c44aa
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2019-04-08 09:57:21 +0200

    Opam repository: Adaptation to hidapi move

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.06.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=9f0956e21f4dcd2803d83072903872eba196bef8
+opam_repository_tag=97f50fc881ef7a74e09825b36d55544cbd3d531a
 full_opam_repository_tag=3ed20d6cfd8a35fd8b459bec3a30e149b6dc03d4
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
