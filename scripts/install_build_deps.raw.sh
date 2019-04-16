e860a7bb1a7522095dacc6c0eba066c4bc16db47
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2019-04-16 18:04:24 +0200

    build-deps: really install and use opam-depext

--- scripts/install_build_deps.raw.sh
+++ scripts/install_build_deps.raw.sh
@@ -9,6 +9,11 @@ opams=$(find "$src_dir/vendors" "$src_dir/src" -name \*.opam -print)
 
 export OPAMYES=${OPAMYES:=true}
 
+## In another ideal world, this list should be extracted from the pinned
+## packages and filter only conf-* packages
+
+opam depext conf-gmp conf-libev conf-m4 conf-perl conf-pkg-config conf-which conf-hidapi
+
 ## In an ideal world, `--with-test` should be present only when using
 ## `--dev`. But this would probably break the CI, so we postponed this
 ## change until someone have some spare time. (@pirbo, @hnrgrgr)
