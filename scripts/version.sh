98d553db9c8843644e7063877b419a2945f25bd3
Raphaël Proust <code@bnwr.net>
2019-04-17 14:15:53 +0800

    version/CI: update opam hash

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=fdc1ab3f23d1601f00a2165e02fcd9fa3ddd8514
+opam_repository_tag=a0ce95cd2089a4d5e8ec9771a15b88a11a44f8ed
 full_opam_repository_tag=2952acf6c0736562d1a94c3fd28ff01a8c38d56b
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
